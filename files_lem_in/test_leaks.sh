f598d22dd3790f89857568c66e6119c020eed50b
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-15 03:37:23 +0200

    Update test_leaks.sh

--- files_lem_in/test_leaks.sh
+++ files_lem_in/test_leaks.sh
@@ -1,5 +1,5 @@
 echo "\n***** TEST LEAKS *****"
-for ((ARG=1; ARG <= 63; ARG+=1))
+for ((ARG=1; ARG <= 64; ARG+=1))
 do
 	echo "----- Test $ARG -----"
 	valgrind --log-file=".testtesttest" --leak-check=full ./lem-in < Correction-tools/files_lem_in/test$ARG.map >> .testtest
