69a5ba2761251415cb2e131e54d9c31d02c30dec
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 10:00:56 +0200

    Update test_leaks.sh

--- files_lem_in/test_leaks.sh
+++ files_lem_in/test_leaks.sh
@@ -2,7 +2,7 @@ echo "\n***** TEST LEAKS *****"
 ((ARG=1))
 for file in Correction-tools/files_lem_in/maps/*
 do
-	echo "----- Test $ARG/247 -----"
+	echo "\r----- Test $ARG/247 -----\c"
 	valgrind --log-file=".testtesttest" ./lem-in < $file >> .testtest
 	grep -e "definitely lost: " -e "indirectly lost: " ".testtesttest" | grep -v " 0 b"
 	((ARG++))
