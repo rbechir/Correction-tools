c532daa6520055163a126ae375904c4a804a7dc9
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 04:04:41 +0200

    Update test_leaks2.sh

--- files_lem_in/test_leaks2.sh
+++ files_lem_in/test_leaks2.sh
@@ -1,9 +1,9 @@
-ARG = 66
+$ARG = 66
 for file in _maps/*
 do
 	echo "----- Test $ARG -----"
 	valgrind --log-file=".testtesttest" --leak-check=full ./lem-in < $file >> .testtest
 	grep -e "definitely lost: " -e "indirectly lost: " ".testtesttest" | grep -v " 0 b"
-	$ARG+=1
+	((ARG++))
 done
 rm -rf .testtest .testtesttest lem-in.dSYM
