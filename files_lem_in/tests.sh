869a42a0b2a43a05dce9bf38020ac02c0ad77239
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 07:45:02 +0200

    Update tests.sh

--- files_lem_in/tests.sh
+++ files_lem_in/tests.sh
@@ -8,12 +8,12 @@ do
 	./lem-in < Correction-tools/files_lem_in/maps/test$ARG.map >> trace 2>&1
 done
 echo "\nTest multipath basique : (30/31/39/40)"
-./lem-in < Correction-tools/files_lem_in/test_sujet3.map | wc -l
-./lem-in < Correction-tools/files_lem_in/test_sujet4.map | wc -l
-./lem-in < Correction-tools/files_lem_in/test_sujet21.map | wc -l
-./lem-in < Correction-tools/files_lem_in/test_sujet22.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_sujet3.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_sujet4.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_sujet21.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_sujet22.map | wc -l
 echo "\nTest multipath avancé : (32/32/51/52)"
-./lem-in < Correction-tools/files_lem_in/test_real_multi3.map | wc -l
-./lem-in < Correction-tools/files_lem_in/test_real_multi4.map | wc -l
-./lem-in < Correction-tools/files_lem_in/test_real_multi42.map | wc -l
-./lem-in < Correction-tools/files_lem_in/test_real_multi43.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_real_multi3.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_real_multi4.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_real_multi42.map | wc -l
+./lem-in < Correction-tools/files_lem_in/maps_multi/test_real_multi43.map | wc -l
