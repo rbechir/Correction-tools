be32522cb1db6e8a2c20570079944d6d611dba8a
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 06:02:23 +0200

    Update tests.sh

--- files_lem_in/tests.sh
+++ files_lem_in/tests.sh
@@ -7,3 +7,7 @@ do
 	echo "Result =>" >> trace
 	./lem-in < Correction-tools/files_lem_in/test$ARG.map >> trace 2>&1
 done
+echo "\nTest multipath basique : "
+./lem-in < Correction-tools/files_lem_in/test_sujet3.map | wc -l
+echo "\nTest multipath avancé : "
+./lem-in < Correction-tools/files_lem_in/test_real_multi.map | wc -l
