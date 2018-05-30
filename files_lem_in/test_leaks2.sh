ff25327296d68816b15b21c4846ae5fd501068de
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 04:00:40 +0200

    Update test_leaks2.sh

--- files_lem_in/test_leaks2.sh
+++ files_lem_in/test_leaks2.sh
@@ -2,7 +2,7 @@ ARG = 66
 for file in _maps/*
 do
 	echo "----- Test $ARG -----"
-	valgrind --log-file=".testtesttest" --leakcheck=full ./lem-in < $file >> .testtest
+	valgrind --log-file=".testtesttest" --leak-check=full ./lem-in < $file >> .testtest
 	grep -e "definitely lost: " -e "indirectly lost: " ".testtesttest" | grep -v " 0 b"
 	$ARG+=1
 done
