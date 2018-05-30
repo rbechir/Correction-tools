546899afb9aa4262b5013575e62561a6ad6bf756
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 03:43:27 +0200

    Update tests.sh

--- files_lem_in/tests.sh
+++ files_lem_in/tests.sh
@@ -1,8 +1,8 @@
 echo "\n***** TESTS *****"
 for ((ARG=1; ARG <= 65; ARG+=1))
 do
-	echo "\n----- Test $ARG -----"
-	cat Correction-tools/files_lem_in/test$ARG.map
-	echo "Result =>"
-	./lem-in < Correction-tools/files_lem_in/test$ARG.map
+	echo "\n----- Test $ARG -----" >> trace
+	cat Correction-tools/files_lem_in/test$ARG.map >> trace
+	echo "Result =>" >> trace
+	./lem-in < Correction-tools/files_lem_in/test$ARG.map >> trace 2>&1
 done
