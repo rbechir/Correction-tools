75117ba76d8d93a16e730f798312044a4df28c4c
rbechir <38251448+rbechir@users.noreply.github.com>
2018-05-30 03:57:51 +0200

    Update lem_in.sh

--- lem_in.sh
+++ lem_in.sh
@@ -6,6 +6,9 @@ cp Correction-tools/files_lem_in/test_leaks.sh .
 zsh test_leaks.sh
 rm test_leaks.sh
 cp -r Correction-tools/files_lem_in/_maps .
+cp Correction-tools/files_lem_in/test_leaks2.sh .
+zsh test_leaks2.sh
+rm test_leaks2.sh
 cp _maps/run.sh .
 sh run.sh
 rm -rf _maps
